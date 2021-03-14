485ad401792682f6d76d9d61ffdc62399e93e78c
Severin Toldo <48881931+severin-toldo@users.noreply.github.com>
2021-03-14 09:24:59 +0100

    added http get function to http service

---
+++ src/model/HttpOptions.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <iterator> 
+#include <utility>
+#include <vector>
+#include "src/shared/PrintUtils.cpp"
+#pragma once
+
+
+class HttpOptions {
+    private: std::vector<std::pair<std::string, std::string>> headers;
+    private: std::vector<std::pair<std::string, std::string>> queryParams;
+
+
+    public: std::vector<std::pair<std::string, std::string>> getHeaders() {
+        return headers;
+    }
+
+    public: void setHeaders(std::vector<std::pair<std::string, std::string>> headers) {
+        this->headers = headers;
+    }
+
+    public: void addHeader(std::string key, std::string value) {
+        std::pair<std::string, std::string> header;
+        header.first = key;
+        header.second = value;
+
+        headers.insert(headers.begin(), header);
+    }
+
+    public: std::vector<std::pair<std::string, std::string>> getQueryParams() {
+        return queryParams;
+    }
+
+    public: void setQueryParams(std::vector<std::pair<std::string, std::string>> queryParams) {
+        this->queryParams = queryParams;
+    }
+
+    public: void addQueryParam(std::string key, std::string value) {
+        std::pair<std::string, std::string> queryParam;
+        queryParam.first = key;
+        queryParam.second = value;
+
+        queryParams.insert(queryParams.begin(), queryParam);
+    }
+};
--- src/service/HttpService.cpp
+++ src/service/HttpService.cpp
@@ -3,7 +3,14 @@
 #include "mbed.h"
 #include "src/shared/PrintUtils.cpp"
 #include "src/shared/ExceptionUtils.cpp"
+#include "MbedJSONValue.h"
+#include "http_request.h"
+#include "src/model/HttpOptions.cpp"
+#include <iterator>
+#include <map>
+#include <string>
 #pragma once
+
 
 class HttpService {
 
@@ -30,12 +37,71 @@ class HttpService {
 
         PrintUtils::print("Connecting Successful");
         PrintUtils::print("MAC: ", wifiInterface->get_mac_address());
-        PrintUtils::print("IP: ", socketAddress.get_ip_address());        
+        PrintUtils::print("IP: ", socketAddress.get_ip_address());
+        PrintUtils::print();      
     }
 
-    public: WiFiInterface* getValueTmp() {
-        return wifiInterface;
+    public: MbedJSONValue get(std::string url) {
+        return get(url, NULL);
     }
 
-    // TODO http functions
+    public: MbedJSONValue get(std::string url, HttpOptions* httpOptions) {
+        if (httpOptions && httpOptions->getQueryParams().size() > 0) {
+            url = url + "?";
+            
+            for (int i = 0; i < httpOptions->getQueryParams().size(); i++) {
+                std::pair<std::string, std::string> queryParam = httpOptions->getQueryParams().at(i);
+                url = url + queryParam.first + "=" + queryParam.second + "&";
+            }
+        }
+
+        PrintUtils::print("url: ", url);
+        
+        HttpRequest* httpRequest = new HttpRequest(wifiInterface, HTTP_GET, url.c_str());
+
+        if (httpOptions) {
+            for (int i = 0; i < httpOptions->getHeaders().size(); i++) {
+                std::pair<std::string, std::string> header = httpOptions->getHeaders().at(i);
+                httpRequest->set_header(header.first, header.second);
+            }
+        }
+        
+        HttpResponse* httpRespone = httpRequest->send();
+
+        if (httpRespone) {
+            MbedJSONValue responseBodyAsJson;
+            parse(responseBodyAsJson, httpRespone->get_body_as_string().c_str());
+            delete httpRequest;
+            
+            return responseBodyAsJson;
+        } else {
+            ExceptionUtils::throwException("Http Request failed with code ", httpRequest->get_error());
+
+            return NULL;
+        }
+    }
+
+
+
+
+    /*
+    public: void tmp2() {
+        HttpRequest* get_req = new HttpRequest( network, HTTP_POST, message );
+
+        HttpResponse* get_res = get_req->send();
+        if (!get_res)
+        {
+            printf("HttpRequest failed (error code %d)\n", get_req->get_error());
+            return 1;
+        }
+        delete get_req;
+    }
+    */
+
+
+
+
+
+
+
 };
